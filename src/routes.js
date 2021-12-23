84c4d47bcfb01012430d236070d82c322a490d0b
DawiMessias <wtih123@gmail.com>
2021-12-23 17:49:32 -0300

    rotas e configs de server

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const routes = express.Router()
 
-routes.get("/", (req, res) => {
-    res.render("index")
-})
+routes.get("/", (req, res) => res.render("index"))
+
+routes.get("/room", (req, res)  => res.render("room"))
+
+routes.get("/create-pass", (req, res) => res.render("create-pass"))           
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
-const server = express()
 const route = require("./routes")
+const path = require("path")
+
+const server = express()
+ 
+server.set("view engine", "ejs")
+server.use(express.static("public"))
+server.set("views", path.join(__dirname, "views"))
 
 server.use(route)
-server.listen(3000, () => console.log("Rodando na Porta: 3000"))
+server.listen(3000, () => console.log(`rodando na porta: 3000`))
 
