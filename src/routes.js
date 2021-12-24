19eccea660066d5060298c8b5b3f111711c6bd00
DawiMessias <wtih123@gmail.com>
2021-12-24 13:29:32 -0300

    Add identificadores e ajuste de rota

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,6 @@ routes.get("/", (req, res) => res.render("index"))
 
 routes.get("/room", (req, res)  => res.render("room"))
 
-routes.get("/create-pass", (req, res) => res.render("create-pass"))           
+routes.get("/create-pass", (req, res) => res.render("create-pass"))
 
 module.exports = routes
