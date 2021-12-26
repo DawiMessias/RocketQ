9c0a9a727fa15c0ccc26413eb90972bc53612a7d
DawiMessias <wtih123@gmail.com>
2021-12-26 10:49:54 -0300

    Começando com constrollers

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const routes = express.Router()
-
+const questionController  = require("./controllers/questionController")
 routes.get("/", (req, res) => res.render("index"))
 
 routes.get("/room", (req, res)  => res.render("room"))
 
 routes.get("/create-pass", (req, res) => res.render("create-pass"))
+
+routes.get("/room/:room/:question/:action", (req, res) => questionController )
 
 module.exports = routes
