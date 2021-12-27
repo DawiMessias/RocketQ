67d22716831271ccf5d31e5339e4f22364d10492
DawiMessias <wtih123@gmail.com>
2021-12-27 16:31:20 -0300

    Configs do DB

--- src/db/config.js
+++ src/db/config.js
@@ -1,8 +1,8 @@
-const sqlite3 = require("sqlite3")
-const { open } = require("sqlite")
+const sqlite3 = require("sqlite3").verbose();
+const { open } = require("sqlite");
 
 module.exports = () => 
     open({
-        filename: "/src/db/rocketq.sqlite",
-        driver: sqlite3.database
+        filename: "./src/db/rocketq.sqlite",
+        driver: sqlite3.Database,
     })
