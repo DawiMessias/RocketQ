6aa481c968276c5d0720317d3bc0a273e215c1ee
DawiMessias <wtih123@gmail.com>
2021-12-27 16:31:38 -0300

    Lógica para gerar número e inserir na base

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,6 +1,25 @@
+const DataBase = require("../db/config")
+
 module.exports = {
-    create(req, res) {
-        let roomId = req.body.password
+    async create(req, res) {
+        const db = await DataBase()
+        const pass = req.body.password
+        let roomId = 0
+
+        for( let i = 0; i < 6; i++) {
+            let igGenerate = Math.floor(Math.random() * 10).toString()
+            i == 0 ? roomId = igGenerate 
+                    : roomId += igGenerate
+        }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+        await db.close()
 
         res.redirect(`/room/${roomId}`)
     }
