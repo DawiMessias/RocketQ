d9b9919ec1e5f550832809ebb3058381b4927d94
DawiMessias <wtih123@gmail.com>
2021-12-27 12:06:58 -0300

    ajustes de rotas / inclusão de midleware

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = express()
 server.set("view engine", "ejs")
 server.use(express.static("public"))
 server.set("views", path.join(__dirname, "views"))
-
+server.use(express.urlencoded({extended: true}))
 server.use(route)
 server.listen(3000, () => console.log(`rodando na porta: 3000`))
 
