e29895f1cceb0182378ef44708507aa56b89b628
DawiMessias <wtih123@gmail.com>
2021-12-29 19:59:58 -0300

    Lógica para gerar id da sala único.

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -5,22 +5,36 @@ module.exports = {
         const db = await DataBase()
         const pass = req.body.password
         let roomId = 0
-
-        for( let i = 0; i < 6; i++) {
-            let igGenerate = Math.floor(Math.random() * 10).toString()
-            i == 0 ? roomId = igGenerate 
-                    : roomId += igGenerate
+        let isRoom = true
+        while(isRoom) {
+            for( var i = 0; i < 6; i++) {
+                let idGenerate = Math.floor(Math.random() * 10).toString()
+                i == 0 ? roomId = idGenerate 
+                        : roomId += idGenerate
+            }
+    
+            const dbRoomsId = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = dbRoomsId.some(dbRoomsId => dbRoomsId === roomId)
+    
+            if (!dbRoomsId) {
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            } 
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
         await db.close()
-
+    
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req, res) {
+        const roomId = req.params.roomId
+        res.render("room", {roomId: roomId})
     }
 }
