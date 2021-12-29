4c55bb2280e6d944ce6272abef855324ca92517c
DawiMessias <wtih123@gmail.com>
2021-12-29 19:59:41 -0300

    Novo Controller

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const questionController  = require("./controllers/questionController")
 const roomController = require("./controllers/roomController")
 routes.get("/", (req, res) => res.render("index", {page: "enter-room"}))
 routes.get("/create-pass", (req, res) => res.render("index", {page: "create-pass"}))
-routes.get("/room/:roomId", (req, res)  => res.render("room"))
+routes.get("/room/:roomId", roomController.open)
 
 routes.post("/question/:room/:question/:action", questionController.index)
 routes.post("/create-room", roomController.create)
