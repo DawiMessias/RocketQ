287a1309b41aa3b464f36356c21d8286b51b0c13
DawiMessias <wtih123@gmail.com>
2021-12-30 20:01:04 -0300

    Ajustes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ const express = require("express")
 const routes = express.Router()
 const questionController  = require("./controllers/questionController")
 const roomController = require("./controllers/roomController")
+
 routes.get("/", (req, res) => res.render("index", {page: "enter-room"}))
 routes.get("/create-pass", (req, res) => res.render("index", {page: "create-pass"}))
-routes.get("/room/:roomId", roomController.open)
 
-routes.post("/question/:room/:question/:action", questionController.index)
+routes.get("/room/:roomId", roomController.open)
 routes.post("/create-room", roomController.create)
+
+routes.post("/question/create/:room", questionController.create)
+routes.post("/question/:room/:question/:action", questionController.index)
 module.exports = routes
