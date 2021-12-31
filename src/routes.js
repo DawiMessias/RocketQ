1eb72a1fc442cd33086b402aed9ee37c1285f9ca
DawiMessias <wtih123@gmail.com>
2021-12-31 15:55:08 -0300

    Rota para poder acessar sala.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 const express = require("express")
-const routes = express.Router()
 const questionController  = require("./controllers/questionController")
 const roomController = require("./controllers/roomController")
+const routes = express.Router()
 
 routes.get("/", (req, res) => res.render("index", {page: "enter-room"}))
 routes.get("/create-pass", (req, res) => res.render("index", {page: "create-pass"}))
 
 routes.get("/room/:roomId", roomController.open)
 routes.post("/create-room", roomController.create)
+routes.post("/enterrom", roomController.enter)
 
 routes.post("/question/create/:room", questionController.create)
 routes.post("/question/:room/:question/:action", questionController.index)
